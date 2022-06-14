b38ab7165e9b73ec730a60167d68759fe54c3ec8
ajnura <ainura.zhanabidinkyzy.ch@gmail.com>
2022-06-14 12:07:43 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList <City>arrayList=new ArrayList<>();
+        arrayList.add(new City(23,"Bishkek"));
+        arrayList.add(new City(78,"Chui"));
+        arrayList.add(new City(-23,"Batken"));
+        arrayList.add(new City(217,"Talas"));
+        arrayList.add(new City(45,"Naryn"));
+        arrayList.add(new City(-23,"Osh"));
+
+        Set<City> set = new TreeSet<>(arrayList);
+        Set<City> set1 = new TreeSet<>(arrayList);
+
+        for (City city : set) {
+            if (city.getCode() % 2 == 1) {
+                set1.add(city);
+                System.out.println(city);
+            }
+        }
+        System.out.println();
+        for (City city : set1) {
+            if (city.getCode() % 2 == 0) {
+                set.add(city);
+                System.out.println(city);
+            }
+        }
+
+
+
+
+    }
+}
